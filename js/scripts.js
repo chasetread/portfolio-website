1334ac93100979d92fc6dc1441bcd9f20356e426
ctreadaw94 <65686060+ctreadaw94@users.noreply.github.com>
2024-01-03 18:12:38 -0600

    Added Collapsing Menu for header; smooth scrolling enabled via CSS;

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello and welcome to Chase's portfolio!")
+function menuToggle() {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+      x.className += " responsive";
+    } else {
+      x.className = "navtoggle";
+    }
+}
